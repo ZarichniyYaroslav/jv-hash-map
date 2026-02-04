3f52e8ea1d7912ef98cd376ac20ebfcae8ab2e8b
jaryk <zarichniyyaroslav@gmail.com>
2026-02-04 19:58:41 +0100

    changed access modificator to private in class node

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -86,7 +86,7 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    static class Node<K, V> {
+    private class Node<K, V> {
         private final K key;
         private V value;
         private Node<K, V> next;
